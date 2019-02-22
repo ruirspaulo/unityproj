6852b150876e1076ba89481f1935db9ffd81a14d
ruirspaulo <47294980+ruirspaulo@users.noreply.github.com>
2019-02-22 09:43:55 +0000

    Add files via upload

---
+++ unityproj-master/New Unity Project/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public int whoturn;//0=x and 1=o
+    public int turncount;//numeor de turnos
+    public GameObject[] turnIcons; //mostra de quem é o turno
+    public Sprite[] playicons;// o=x icon e 1=y icoon
+    public Button[] tictactoeSpaces; //espaço jogavel
+
+	// Use this for initialization
+	void Start () {
+        GameSetup();
+	}
+
+    void GameSetup() {
+        whoturn = 0;
+        turncount = 0;
+        turnIcons[0].SetActive(true);
+        turnIcons[1].SetActive(false);
+        for (int i = 0; i < tictactoeSpaces.Length; i++) {
+            tictactoeSpaces[i].interactable = true;
+            tictactoeSpaces[1].GetComponent<Image>().sprite=null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+	}
+}
